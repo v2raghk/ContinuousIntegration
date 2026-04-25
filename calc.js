4f0e01226e9de934a54671118694b58baab04d49
v2raghk <raghav.kapoor@hotmail.co.uk>
2026-04-25 16:55:22 +0100

    Removed unnecessary files

--- calc.js
+++
@@ -1,9 +0,0 @@
-function add(a, b) { return a + b; }
-function sub(a, b) { return a - b; }
-function mul(a, b) { return a * b; }
-function div(a, b) { return a / b; }
-
-console.log("Add:", add(2, 3));
-console.log("Sub:", sub(5, 2));
-console.log("Mul:", mul(3, 4));
-console.log("Div:", div(10, 2));
--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("CI practice run - Hello, World!");
-// updated comment: testing CI trigger
